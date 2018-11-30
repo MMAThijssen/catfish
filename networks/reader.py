13ab3c824f6a3b6c093aff493f0f22bf17b25f82
Marijke <marijke.thijssen@wur.nl>
2018-11-30 15:35:18 +0100

    Added spacing for readability

--- networks/reader.py
+++ networks/reader.py
@@ -19,3 +19,17 @@ def load_npz(npz_file):
         x = npz["raw"]
         y = npz["base_labels"]                                                  # labels are 0 (non-HP) or 1 (HP)
     return x, y
+
+
+def load_npz_labels(npz_file):
+    """
+    Retrieves information saved in NPZ file.
+    
+    Args:
+        npz_file -- str, path to .npz file
+        
+    Returs: tuple (np.array of raw signal, np.array of labels)
+    """
+    with np.load(npz_file) as npz:
+        labels = npz["base_labels"]                                                  # labels are 0 (non-HP) or 1 (HP)
+    return labels
