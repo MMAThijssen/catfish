61e47e3081f59c8b14ae0e23af9b693f30cd72f3
Marijke <marijke.thijssen@wur.nl>
2019-01-07 11:35:54 +0100

    main script for tool

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Main script to run tool from 
+"""
+
+# 1. Get input from user:
+#       * fast5 file
+#       * output file name
+#       * specify some options?
+#       * specify some output?
+
+# 2. Infer signal
+
+# 3. Save to file
+
+# 4. Give output message
