635056086410c7fb23553b51248e4a79b1c64010
Marijke <marijke.thijssen@wur.nl>
2019-02-01 11:30:01 +0100

    script to split fast5 files

---
+++ tool/split_f5.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import h5py
+import os.path
+from shutil import copyfile
+from sys import argv
+
+
+def split_signal(input_file, splits):
+    """
+    Splits raw signal of FAST5 file on specified entries. All other groups are copied.
+    
+    Args:
+        input_file -- str, path to input file
+        splits -- list of tuple, start and end value to split on; end is inclusive
+    
+    Returns: list of new file names
+    """
+    new_files = []
+    
+    # get signal
+    try:
+        source = h5py.File(input_file, "r")
+    except IOError:
+        raise IOError("ERROR - could not open file, likely corrupted.")  
+          
+    try:
+        read_name = list(source["Raw"]["Reads"])[0]
+        read_attributes = list(source["Raw"]["Reads"][read_name].attrs)
+        signal_dset = source["Raw"]["Reads"][read_name]["Signal"]
+    except:
+        raise RuntimeError("ERROR - no raw signal data was stored in file.")
+    
+    index = 0
+    # split signal          # ADJUST TO IMMEDIATELY MAKE NEW FILE
+    for s in splits:
+        new_signal = signal_dset[s[0] : s[1] + 1]
+        
+        # make file
+        dest_name = "{}_{}.fast5".format(os.path.basename(input_file).split(".")[0], index)
+        copyfile(input_file, dest_name)     # TODO: change dest_name to path within dest dir
+        dest = h5py.File(dest_name, "r+")
+        raw_reads = dest["Raw"]["Reads"][read_name]
+        
+        # replace signal with split signal
+        del dest["Raw"]["Reads"][read_name]["Signal"]
+        raw_reads.create_dataset("Signal", data=new_signal, dtype="int16", 
+                                 compression="gzip",compression_opts=9)
+        dest.close()
+        index += 1
+        new_files.append(dest_name)
+    
+    return new_files
+
+    
+if __name__ == "__main__":
+    fast5 = argv[1]
+    splits = [(0, 100), (200, 300)]
+    print(split_signal(fast5, splits))
+    
+    
+    
+    ### OUTPUTS ###
+    # list(source.keys()) > ['Analyses', 'Raw', 'UniqueGlobalKey']
+    # raw_group.shape, raw_group.dtype > 'Group' object has no attribute 'shape', 'dtype'
+    # list(raw_group.keys()) > ['Reads']
+    # source["Raw"]["Reads"] > ['Read_3141']
+    # signal_dset.shape, signal_dset.dtype > ((226716,), dtype('int16'))
+
+
+
+
